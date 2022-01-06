bdd9c1cfdc9bc4a0fccf021c737b208582afbe33
owlru <valujenich@inbox.ru>
2022-01-07 02:43:20 +0300

    Переделанная домашка 1.9

--- src/ru/sky/pro/Main.java
+++ src/ru/sky/pro/Main.java
@@ -1,8 +1,25 @@
 package ru.sky.pro;
 
+import ru.sky.pro.lesson1_9.Autor;
+import ru.sky.pro.lesson1_9.Book;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Autor rouling = new Autor("Джоан", "Роулинг");
+        Autor tolkin = new Autor("Джон", "Толкин");
+        System.out.println(rouling.getFirstName() + " " + rouling.getSecondName());
+
+        Book harryPotterAndPhilosopherSStone = new Book(rouling,"Гарри Поттер и филосовский камень", 1997);
+        Book theLordOfTheRings = new Book(tolkin, "Властелин колец", 1954);
+
+        System.out.println(harryPotterAndPhilosopherSStone);
+        System.out.println(theLordOfTheRings);
+
+        System.out.println("Изменение года издания:");
+        harryPotterAndPhilosopherSStone.setYearBook(2001);
+        System.out.println(harryPotterAndPhilosopherSStone);
+
     }
 }
